caea97b8c225519d4d075ce44b42476dd2c0a653
Yann Mainier <yann.mainier@gmail.com>
2023-09-18 21:18:47 +0100

    max width

--- app/components/VideoList.tsx
+++ app/components/VideoList.tsx
@@ -16,7 +16,7 @@ export default async function VideoList({
   const fileTree = buildFileTree(sortedFiles, dir);
 
   return (
-    <main className="p-6 pt-12 sm:p-12">
+    <main className="mx-auto max-w-3xl p-6 pt-12 sm:p-12">
       <div className="flex flex-col sm:flex-row justify-between sm:items-center pb-6 sm:pb-12">
         <h1 className="text-2xl">{title}</h1>
         <Link href="/" className="mt-4 sm:mt-0 bg-transparent hover:bg-red-700 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-700 hover:border-transparent text-center rounded">back</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LinkButton from "./components/LinkButton";
 
 export default function Home() {
   return (
-    <main className="flex flex-col sm:flex-row items-center sm:items-start min-h-screen p-6 pt-12 sm:p-12 gap-6 sm:gap-12">
+    <main className="mx-auto max-w-3xl flex flex-col sm:flex-row items-center sm:items-start min-h-screen p-6 pt-12 sm:p-12 gap-6 sm:gap-12">
       <LinkButton href="/tvshows" className="w-full text-center">TV&nbsp;Shows</LinkButton>
       <LinkButton href="/movies" className="w-full text-center">Movies</LinkButton>
     </main>
