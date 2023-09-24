8cf331dba34c9155b064ffff246ef62680281038
Yann Mainier <yann.mainier@gmail.com>
2023-09-24 15:43:03 +0100

    force revalidation on tvshows and movies

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,5 +1,8 @@
 import VideoList from "../components/VideoList";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function TvShows() {
   return <VideoList title="Movies" path={process.env.NEXT_PUBLIC_MOVIES_ROOT ?? '/'} />;
 }
--- app/tvshows/page.tsx
+++ app/tvshows/page.tsx
@@ -1,5 +1,8 @@
 import VideoList from "../components/VideoList";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function TvShows() {
   return <VideoList title="TV Shows" path={process.env.NEXT_PUBLIC_SHOWS_ROOT ?? '/'} />;
 }
